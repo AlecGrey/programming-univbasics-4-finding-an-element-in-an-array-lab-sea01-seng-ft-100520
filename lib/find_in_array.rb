96f52ec4fb0708bf5cb3e6733b0614659bf603f0
Alexander Grey <alecgrey.coding@gmail.com>
2020-08-06 22:40:06 +0000

    Done.

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,14 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  counter = 0
+  while array.length >= counter do
+    if array[counter] == value_to_find
+      return counter
+    else
+      counter += 1
+    end
+  end
 end
+
+array = "sheet beet YEET feet meet greet".split(" ")
+
+puts find_element_index(array, "YEET")
